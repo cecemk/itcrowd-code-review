102b9f66f22554501b1b3bc553edb23c21dcee80
Stefan Trsunov <74921412+cecemk@users.noreply.github.com>
2021-09-26 19:50:14 +0200

    Create ladder.py

---
+++ ladder.py
@@ -0,0 +1,4 @@
+//Imas skali,pocnuvas od 0-tata skala i treba da se iskacis do gore. Na sekoja skala ima broj napisan po red 1....n vo eden cekor mozis da skoknis ili 1 ili 2 skali, kako so
+//se kacvas po sklaite gi sobiras rednite broevi za daden broj n so e brojot na skali, treba da vratis maximum suma kolku mozes da soberes. Sekoja skala ima i svoja vrednost 
+//Treba da se resi so brute force, a ovaj nacin na resavanje e greedy(neraboti)
+
